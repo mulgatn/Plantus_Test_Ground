17930217990ab86604ad72c090e1bedb985ab5a2
Peace <kbarisbicak@hotmail.com>
2020-03-23 20:04:48 +0100

    Initial Commit, the repo is set

---
+++ PlantusTestGround/Intermediate/Build/Win64/PlantusTestGroundEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "D:/UE_Projects/Plantus_Test_Ground/PlantusTestGround/Intermediate/Build/Win64/PlantusTestGroundEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ PlantusTestGround/Intermediate/Build/Win64/UE4Editor/Inc/PlantusTestGround/PlantusTestGroundGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PLANTUSTESTGROUND_PlantusTestGroundGameModeBase_generated_h
+#error "PlantusTestGroundGameModeBase.generated.h already included, missing '#pragma once' in PlantusTestGroundGameModeBase.h"
+#endif
+#define PLANTUSTESTGROUND_PlantusTestGroundGameModeBase_generated_h
+
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_SPARSE_DATA
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_RPC_WRAPPERS
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPlantusTestGroundGameModeBase(); \
+	friend struct Z_Construct_UClass_APlantusTestGroundGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(APlantusTestGroundGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/PlantusTestGround"), NO_API) \
+	DECLARE_SERIALIZER(APlantusTestGroundGameModeBase)
+
+
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAPlantusTestGroundGameModeBase(); \
+	friend struct Z_Construct_UClass_APlantusTestGroundGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(APlantusTestGroundGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/PlantusTestGround"), NO_API) \
+	DECLARE_SERIALIZER(APlantusTestGroundGameModeBase)
+
+
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APlantusTestGroundGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APlantusTestGroundGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APlantusTestGroundGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APlantusTestGroundGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APlantusTestGroundGameModeBase(APlantusTestGroundGameModeBase&&); \
+	NO_API APlantusTestGroundGameModeBase(const APlantusTestGroundGameModeBase&); \
+public:
+
+
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APlantusTestGroundGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APlantusTestGroundGameModeBase(APlantusTestGroundGameModeBase&&); \
+	NO_API APlantusTestGroundGameModeBase(const APlantusTestGroundGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APlantusTestGroundGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APlantusTestGroundGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APlantusTestGroundGameModeBase)
+
+
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_12_PROLOG
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_SPARSE_DATA \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_RPC_WRAPPERS \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_INCLASS \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_SPARSE_DATA \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PLANTUSTESTGROUND_API UClass* StaticClass<class APlantusTestGroundGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID PlantusTestGround_Source_PlantusTestGround_PlantusTestGroundGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
